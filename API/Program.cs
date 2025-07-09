a551417dd8646d4988d3e29829449a1f93f87524
Huzaifa <huzaifajameel2@gmail.com>
2025-07-09 15:09:36 +0500

    End of Section 1

---
+++ API/Program.cs
@@ -0,0 +1,26 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+
+builder.Services.AddDbContext<StoreContext>(option => {
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+
+var app = builder.Build();
+
+
+
+// Configure the HTTP request pipeline.
+
+
+app.MapControllers();
+
+app.Run();
