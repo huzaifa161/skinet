0376a651c6521f6263c21ac68a20dbf2d048d9d1
Huzaifa <huzaifajameel2@gmail.com>
2025-07-16 22:18:53 +0500

    End of Section 4

--- API/Program.cs
+++ API/Program.cs
@@ -16,6 +16,7 @@ builder.Services.AddDbContext<StoreContext>(option =>
 });
 
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped(typeof(IGenericRepository<>),typeof(GenericRepository<>));
 
 
 var app = builder.Build();
