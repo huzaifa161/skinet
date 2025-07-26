0f7d64ffd1c634a79f392712b16eca2a3ed7d61c
Huzaifa <huzaifajameel2@gmail.com>
2025-07-26 21:12:21 +0500

    End of section 6

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using API.Middleware;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
@@ -16,7 +17,8 @@ builder.Services.AddDbContext<StoreContext>(option =>
 });
 
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
-builder.Services.AddScoped(typeof(IGenericRepository<>),typeof(GenericRepository<>));
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddCors();
 
 
 var app = builder.Build();
@@ -25,7 +27,8 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-
+app.UseMiddleware<ExceptionMiddleware>();
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https//:localhost:4200"));
 app.MapControllers();
 
 try
